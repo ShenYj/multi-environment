195ccd520a40b7ce9e4a7c12db252d36a12318f2
ShenYj <shenyanjie123@foxmail.com>
2021-01-18 16:17:28 +0800

    xcconfig多环境变量

---
+++ MultiEnvByXCConfig/MultiEnvByXCConfig/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  MultiEnvByXCConfig
+//
+//  Created by EZen on 2021/1/18.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *describeLabel;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSString *hostURL = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"HOST_URL"];
+    self.describeLabel.text = hostURL;
+    
+}
+
+
+@end
